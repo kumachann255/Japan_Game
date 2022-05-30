041d36070e579f1e964f68b32537bed8b312267d
OtsukaMasanori <hal.otsuka.masanori.2021@gmail.com>
2022-05-30 18:42:51 +0900

    kooo

--- countdown.cpp
+++ countdown.cpp
@@ -142,28 +142,17 @@ HRESULT InitCountDown(void)
 	{
 	case stage2:
 		g_Use2[2] = TRUE;
-		// SEçƒê∂
-		PlaySound(SOUND_LABEL_SE_cheers05);
-
-		break;
 
 	case stage1:
 		g_Use2[1] = TRUE;
-		// SEçƒê∂
-		PlaySound(SOUND_LABEL_SE_cheers05);
-
-		break;
 
 	case stage0:
 		g_Use2[0] = TRUE;
-		// SEçƒê∂
-		PlaySound(SOUND_LABEL_SE_cheers05);
-
-		break;
 
 	}
-	// BGMçƒê∂
-	//PlaySound(SOUND_LABEL_BGM_sample000);
+
+	// SEçƒê∂
+	PlaySound(SOUND_LABEL_SE_cheers05);
 
 	g_Load = TRUE;
 	return S_OK;
